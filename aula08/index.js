3b736fc0882a38179daa7b116a4d9e84d329e4f3
bielgeri <bielgeri@gmail.com>
2023-01-02 09:00:25 -0300

    correção

--- aula08/index.js
+++ aula08/index.js
@@ -5,5 +5,5 @@ const peso = 62
 const altura = 1.73
 let imc = peso / (altura * altura)
 let anoNascimento =  2023 - idade
-console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg, tem', altura, 'de altura e seu IMC é de ', imc )
-console.log(nome, sobrenome, 'nasceu em ', anoNascimento)
+console.log(`${nome} ${sobrenome} tem ${idade} anos pesa ${peso} kg tem ${altura} de altura e seu IMC é de ${imc}` )
+console.log(`${nome} ${sobrenome} nasceu em  ${anoNascimento}`)
