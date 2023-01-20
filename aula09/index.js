f98b51e3cde9bbaf5172dfc401cc28418c7b2268
bielgeri <bielgeri@gmail.com>
2023-01-20 07:39:43 -0300

    aula10

---
+++ aula09/index.js
@@ -0,0 +1,9 @@
+// String, number, undefined, null. boolean
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória
+const aprovado = false; // Boolean = true, false (logico)
---
+++ aula10/index.js
@@ -0,0 +1,23 @@
+/*
+/ Aritméticos 
++ Adição / Concatenação
+** Potenciação
+% Resto da divisão
+*/
+
+/* Ordem de precedência
+- ()
+- **
+- * / %
+- + -
+*/
+
+/*
+Nan = Not a Number parseInt(inteiro), parseFloat(decimais), Number(geral) 
+*/
+
+
+const num1 = 10
+const num2= Number('Luiz');
+console.log(num1 + num2)
+console.log(typeof num2)
