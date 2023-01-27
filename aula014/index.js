1402c21d842f04cf5eaeba1788c3b67446903fff
bielgeri <bielgeri@gmail.com>
2023-01-27 07:30:03 -0300

    numbers js

---
+++ aula014/index.js
@@ -0,0 +1,20 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+
+num1 = parseFloat(num1.toFixed(2))
+
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+//console.log(num1.toString() + num2)
+//num1= num1.toString
+//console.log(num1.toString(2))
+//console.log(num1.toFixed(4))
+//console.log(Number.isInteger(num1))
+//console.log(Number.isNaN(temp))
+//let temp = num1 * 'Ola';
