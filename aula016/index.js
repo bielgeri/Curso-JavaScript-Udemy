dcf2baefadad8e42e384c316a0457809e62a3467
bielgeri <bielgeri@gmail.com>
2023-01-31 08:10:58 -0300

    arrays_js

---
+++ aula016/index.js
@@ -0,0 +1,18 @@
+
+const nome = 'Luiz Otavio'
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+alunos.push('Fabio');
+
+//console.log(alunos.slice(0,3));
+//delete alunos[1];
+
+//console.log(alunos[1]);
+//alunos.push('Luiza') // Adiciona no fim
+//alunos.unshift('Gabriel');
+
+console.log(alunos)
+
+// alunos[0] = 'Eduardo'; = Altera
+//alunos[3] = 'Luiza'; = Adiciona 
