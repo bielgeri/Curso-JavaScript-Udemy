6509a5875fa20c74679d7a55327147a3aa0a2bc5
bielgeri <bielgeri@gmail.com>
2023-02-06 09:19:21 -0300

    exercicio array js

--- aula020/js/script.js
+++ aula020/js/script.js
@@ -1,13 +1,30 @@
 function meuEscopo () {
     const form = document.querySelector('.form')
-
-    let contador = 1
-    function recebeEventoForm(evento) {
-        evento.preventDefault();
-        console.log(`Form não foi enviado! ${contador}`)
-        contador++;
-    }
+    const resultado = document.querySelector('.resultado')
     
+    const pessoas = [];
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas)      
+        resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value}` + `
+        ${peso.value} ${altura.value}</p>`  
+    }
+
+
     form.addEventListener('submit', recebeEventoForm);
+    
 }
 meuEscopo();
