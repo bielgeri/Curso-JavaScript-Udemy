d82bf89c1ca13c0b492176c856de74423f9fdedc
bielgeri <bielgeri@gmail.com>
2023-02-10 09:03:23 -0300

    modelo_css

---
+++ aula025/script.js
@@ -0,0 +1,10 @@
+const numero = 7;
+if (numero >= 0 && numero <= 5 ) {
+    console.log('O numero está entre 0 e 5.')
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O numero está entre 6 e 8.')
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O numero está entre 9 e 11.')
+} else {
+    console.log('O numero não está entre 0 e 11.')
+}
