4b3e2e389aad9faf63eb5af5ff6bbfaf96d98f1f
bielgeri <bielgeri@gmail.com>
2023-03-03 08:40:31 -0300

    Continue e Break

---
+++ aula041/script.js
@@ -0,0 +1,3 @@
+// Escreva uma função que recebe 2 numeros e retorne o maior deles 
+const max2 = (x, y) => x > y ? x : y
+console.log(max2(123, 50))
---
+++ aula042/script.js
@@ -0,0 +1,4 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura
+// de uma imagem (number); Retorne true se a imagem estiver no modo paisagem.
+const ePaisagem = (largura, altura) => largura > altura;
+console.log(ePaisagem(1920, 1080))
