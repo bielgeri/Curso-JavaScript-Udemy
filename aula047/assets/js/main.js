a8da7159655c0351fc3a2271346ef876c8ad86d1
bielgeri <bielgeri@gmail.com>
2023-03-08 09:16:04 -0300

    timer

---
+++ aula047/assets/js/main.js
@@ -0,0 +1,43 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-Br', {
+        timeZone: 'GMT'
+    });
+}
+
+
+const relogio = document.querySelector('.relogio')
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        segundos++
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)
+};
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    
+    if (el.classList.contains('inciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+        
+    }
+
+
+    if (el.classList.contains('zerar')) {
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
+    
+
+    if (el.classList.contains('pausar')) {
+        clearInterval(timer);
+        relogio.classList.add('pausado');   
+    }
+});
+
