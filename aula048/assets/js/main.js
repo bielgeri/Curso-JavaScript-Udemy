47d2e359396217175d117e2757ed0e3533c280d0
bielgeri <bielgeri@gmail.com>
2023-03-13 20:39:09 -0300

    mini projeto tarefa finalizado

--- aula048/assets/js/main.js
+++ aula048/assets/js/main.js
@@ -20,11 +20,23 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botãoApagar = document.createElement('button')
+    botãoApagar.innerText = 'Apagar'
+    botãoApagar.setAttribute('class', 'apagar')
+    botãoApagar.setAttribute('title', 'apagar esta mensagem')
+    li.appendChild(botãoApagar)
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput
     tarefas.appendChild(li);
     limpaInput();
+    criaBotaoApagar(li)
+    salvarTarefas();
+
 }
 
 btnTarefa.addEventListener('click', function() {
@@ -32,3 +44,37 @@ btnTarefa.addEventListener('click', function() {
     criaTarefa(inputTarefa.value)
     
 })
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    console.log(el)
+
+    if(el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaTarefas)
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaTarefas = JSON.parse(tarefas);
+    
+    for (let tarefa of listaTarefas) {
+        criaTarefa(tarefa);
+    } 
+};
+
+adicionaTarefasSalvas();
