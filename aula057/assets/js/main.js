e752865b77c921ec12546f69838deca6a47c5254
bielgeri <bielgeri@gmail.com>
2023-03-27 09:25:05 -0300

    mini projeto calculadora

---
+++ aula057/assets/js/main.js
@@ -0,0 +1,26 @@
+function criaCalculadora() {
+    return { 
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+                
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
