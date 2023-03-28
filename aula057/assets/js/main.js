f50ab5dcf87e558382839c5c9e78589d19b855ee
bielgeri <bielgeri@gmail.com>
2023-03-28 08:54:03 -0300

    update projeto

--- aula057/assets/js/main.js
+++ aula057/assets/js/main.js
@@ -4,6 +4,42 @@ function criaCalculadora() {
 
         inicia() {
             this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keycode === 13) {
+                    this.realizaConta();
+                }
+            })
+        },
+
+        clearDisplay() {
+            this.display.value = ' '
+        },
+
+
+        apagaUm() {
+            this,this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta)
+
+                if (!conta) {
+                    alert('Conta inválida')
+                    return
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta inválida')
+                return
+            }
         },
 
         cliqueBotoes() {
@@ -13,6 +49,19 @@ function criaCalculadora() {
                 if (el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
                 }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+                this.display.focus();
             });
         },
 
