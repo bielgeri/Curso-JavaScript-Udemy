821260492822d8136721e317bf2744ff2b2ae167
bielgeri <bielgeri@gmail.com>
2023-04-05 08:54:45 -0300

    arrays basico

---
+++ aula062/script.js
@@ -0,0 +1,10 @@
+// Valor por referência
+//const nomes = ['Eduardo', 'Flavia', 'Marina', 'Wallace', 'Rosana'];
+//const novo = nomes.slice(1,-1);
+//console.log(novo)
+
+//const nome = 'Luiz Otavio Rodrigues';
+//const nomes = nome.split(' ')
+const nomes = ['Gabriel', 'Rodrigues', 'Geribola']
+const nome = nomes.join(' ');
+console.log(nome)
