0ae5152c13ab1fc1bcb0803900d58d09552bf0ad
bielgeri <bielgeri@gmail.com>
2023-04-06 08:40:09 -0300

    metodo splice

---
+++ aula063/script.js
@@ -0,0 +1,14 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+
+//Push
+nomes.splice(0, 0, 'Luiz');
+console.log(nomes)
+
+//nomes.splice(indíce, delete, elem1, elem2, elem3)
+// pop
+//const removidos = nomes.splice(-1, 1)
+
+//shift
+//const removidos = nomes.splice(0,1)
+
+
