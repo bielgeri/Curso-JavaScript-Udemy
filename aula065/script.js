25e3ec1e1db20dd3ff88971a6ef21c3a2e067690
bielgeri <bielgeri@gmail.com>
2023-04-10 09:06:52 -0300

    metodo.filter( )

---
+++ aula065/script.js
@@ -0,0 +1,32 @@
+// Filter -> Sempre retornar um array, com a mesma quantidade de elementos ou menos
+// Retorne os numeros maiores que 10
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 27];
+// Função de callback
+const numerosFiltrados = numeros.filter(valor => valor > 10);
+console.log(numerosFiltrados)
+
+
+
+// Retorne as pessoas que tem o  nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos;
+// Retorne as pessoas cuja nome termina com a
+const pessoas = [
+    { nome: 'Luiz', idade: 62},
+    { nome: 'Maria', idade: 23},
+    { nome: 'Eduardo', idade: 55},
+    { nome: 'Leticia', idade: 19},
+    { nome: 'Rosana', idade: 32},
+    { nome: 'Wallace', idade: 47}
+]
+
+const pessoasComNomeGrande = pessoas.filter(obj =>  obj.nome.length >= 5);
+const pessoasComMaisde50 = pessoas.filter(obj => obj.idade > 50);
+const nomeTerminaComA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'));
+
+console.log(pessoasComNomeGrande)
+console.log('=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+console.log(pessoasComMaisde50)
+console.log('=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+console.log(nomeTerminaComA)
