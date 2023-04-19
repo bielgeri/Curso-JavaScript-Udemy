714d6644f8d6bf70a427f7261b8e23880e8de791
bielgeri <bielgeri@gmail.com>
2023-04-19 08:40:43 -0300

    get/setter

--- aula071/script.js
+++ aula071/script.js
@@ -4,16 +4,39 @@ function Produto(nome, preco, estoque) {
     this.preco = preco;
    
 
+    let estoquePrivado = estoque;
     Object.defineProperty(this, 'estoque', {
         enumerable: true, // mostra a chave 
-        value: estoque, // valor 
-        writable: true, // pode alterar o valor
-        configurable: false // configurável
+        configurable: false, // configurável
+        get: function() {
+            return estoquePrivado;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('Mensagem de erro!')
+            }
+            estoquePrivado = valor;
+        }
     });
 }
 
-const p1 = new Produto('Camiseta', 20, 3)
-console.log(Object.keys(p1))
-for (let chave in p1) {
-    console.log(chave)
+function criaProduto(nome) {
+    return {
+        get nome () {
+            return nome;
+        },
+        set nome (valor) {
+            valor = valor.replace('coisa', '')
+            nome = valor
+        }
+    };
 }
+
+//const p1 = new Produto('Camiseta', 20, 3)
+// console.log(p1)
+//p1.estoque = 'Olá mundo rs'
+//console.log(p1.estoque)
+const p2 = criaProduto('Camiseta')
+p2.nome = 'Qualquer coisa'
+console.log(p2.nome)
+
