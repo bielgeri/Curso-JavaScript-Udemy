d4b3cbd76b761e16eda68b6b7003a83a1b03316b
bielgeri <bielgeri@gmail.com>
2023-05-12 08:44:51 -0300

    Validando Formulario

---
+++ aula086 Formulario/modelo/assets/js/main.js
@@ -0,0 +1,42 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario')
+        this.eventos();
+    }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+            errorText.remove();
+        }
+        
+        for (let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText;
+
+            if(!campo.value) {
+                this.criaErro(campo, `Campo ${label} não pode estar em branco`)
+                valid = false;
+            }
+        } 
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div)
+    }
+}
+
+const valida = new ValidaFormulario();
