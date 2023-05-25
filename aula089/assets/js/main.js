940139f3cd48418a70ff60b1fac751bc5ee869aa
bielgeri <bielgeri@gmail.com>
2023-05-25 09:01:44 -0300

    Fetch API

--- aula089/assets/js/main.js
+++ aula089/assets/js/main.js
@@ -1,18 +1,4 @@
-const request = obj => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(obj.method, obj.url, true);
-        xhr.send();
-    
-        xhr.addEventListener('load', () => {
-            if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.responseText);
-            } else {
-                reject(xhr.statusText)
-            }
-        });
-    });      
-};
+
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -24,26 +10,24 @@ document.addEventListener('click', e => {
     }
 })
 
-async function carregaPagina(el) {
-    const href = el.getAttribute('href');
-    
-    const objConfig = {
-        method: 'GET',
-        url: href,
-    };
-
+ async function carregaPagina(el) {
     try {
-        const response = await request(objConfig);
-        carregaResultado(response)
+        const href = el.getAttribute('href');
+    const response = await fetch(href);
+
+    if (response.status !== 200) throw new Error('ERREI FUI MLK.')
+
+    const html = await response.text();
+
+    carregaResultado(html)
     } catch(e) {
         console.log(e)
     }
-
-   
-  
+    
 }
 
 function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 } 
+
