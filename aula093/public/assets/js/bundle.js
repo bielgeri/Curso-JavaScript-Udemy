302891ee86dd5c27307f18dd12e7a0f6809a1cb8
bielgeri <bielgeri@gmail.com>
2023-05-31 09:09:34 -0300

    webpack (E)

---
+++ aula093/public/assets/js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Unknown option: .preset. Check out https://babeljs.io/docs/en/babel-core/#options for more information about options.\n    at validate (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\@babel\\core\\lib\\config\\validation\\options.js:86:25)\n    at loadPrivatePartialConfig (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\@babel\\core\\lib\\config\\partial.js:58:50)\n    at loadPrivatePartialConfig.next (<anonymous>)\n    at Object.<anonymous> (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\@babel\\core\\lib\\config\\partial.js:122:25)\n    at Generator.next (<anonymous>)\n    at step (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\gensync\\index.js:261:32)\n    at evaluateAsync (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\gensync\\index.js:291:5)\n    at C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\gensync\\index.js:93:9\n    at new Promise (<anonymous>)\n    at Object.async (C:\\Users\\Gabriel Geribola\\Documents\\cursoTI\\Curso-JavaScript-Udemy\\aula093\\node_modules\\gensync\\index.js:92:14)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
