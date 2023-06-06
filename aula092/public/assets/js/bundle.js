f50e9bc5df95af1c563471086549c52c0ae6df96
bielgeri <bielgeri@gmail.com>
2023-06-06 09:17:44 -0300

    Import / Export

---
+++ aula092/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var o=new class{constructor(o,s){this.nome=o,this.sobrenome=s}}("Gabriel","Geribola");console.log(o)})();
+//# sourceMappingURL=bundle.js.map
---
+++ aula092/src/modulo1.js
@@ -0,0 +1,21 @@
+
+export const nome = "Gabriel"
+export const sobrenome = "Geribola"
+export const idade = 18;
+export const cpf = "~cpf"
+
+export function soma(x, y) {
+    return x + y;
+}
+
+export default class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+ 
+
+
+
+
