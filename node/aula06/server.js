4a3ea3edbee202aece70605842e9bab89e253f52
bielgeri <bielgeri@gmail.com>
2023-06-30 08:56:06 -0300

    express introdução (E)

--- node/aula06/server.js
+++ node/aula06/server.js
@@ -1,30 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-
-// CRUD -> CREATE,  READ,  UPTADE,  DELETE
-//          POST     GET     PUT    DELETE
-
-// hhtps://meusite.com/ <- GET -> Entregue a página
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(`
-    <form action="/" method="POST>
-    Nome: <input type="text" name="nome>
-    <button>Enviar</button>
-    </form>
-    `);
-});
-
-app.get('/contato', (req, res) => {
-    res.send('Obrigado por entrar em contato com a gente!')
-});
-
-app.post('/', (req, res) => {
-    res.send('Recebi o formulário')
+    res.send('Hello Word');
 });
 
 app.listen(3000, () => {
-    console.log('Acessar  https://localhost:3000')
+    console.log('Acessar https://localhost:3000')
     console.log('Servidor executando na porta 3000')
 });
