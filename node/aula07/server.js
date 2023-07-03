de980a425dcd65f6d62f98c44c1000d21ce16280
bielgeri <bielgeri@gmail.com>
2023-07-03 09:06:30 -0300

    nodemon

---
+++ node/aula07/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome do cliente: <input type="text" name="nome">
+    <button>Olá mundo </button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário')
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente!')
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000')
+});
