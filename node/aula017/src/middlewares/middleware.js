66281f683c37c9dbafa340f525d80478460a396e
bielgeri <bielgeri@gmail.com>
2023-07-14 08:56:30 -0300

    helmet e csrf

---
+++ node/aula017/src/middlewares/middleware.js
@@ -0,0 +1,18 @@
+exports.middleWareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este é o valor da variavel local'
+    next();
+}
+exports.outroMiddleware = (req, res, next) => {
+    next();
+}
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code  ) {
+        return res.render('404')
+    }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken()
+    next();
+}
