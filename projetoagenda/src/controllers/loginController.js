256f839f62f9066b9ee71a2ef30b78cbc1569a96
bielgeri <bielgeri@gmail.com>
2023-07-24 09:20:53 -0300

    logando o usuario

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -2,7 +2,8 @@ const { async } = require('regenerator-runtime');
 const Login = require('../models/loginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado')
+    return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -28,3 +29,32 @@ exports.register = async function(req, res) {
     }
     
 };
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+            await login.login();
+
+            if(login.errors.length > 0) {
+                req.flash('errors', login.errors);
+                req.session.save(function() {
+                    return res.redirect('/login/index');
+                });
+                return;
+            }
+
+            req.flash('success', 'Você entrou no sistema.');
+            req.session.user = login.user;
+            req.session.save(function() {
+                return res.redirect('/login/index');
+            });
+    } catch(e) {
+        console.log(e)
+        return res.render('404')
+    }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
--- projetoagenda/src/models/loginModel.js
+++ projetoagenda/src/models/loginModel.js
@@ -16,6 +16,23 @@ class Login  {
         this.user = null;
     }
 
+    async login() {
+        this.valida();
+        if(this.errors.length > 0) return;
+        this.user =  await LoginModel.findOne({email: this.body.email});
+
+        if(!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha inválida.')
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.valida();
         if(this.errors.length > 0) return;
@@ -23,23 +40,19 @@ class Login  {
         await this.userExists();
 
         if(this.errors.length > 0) return;
-        
+
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            
-            this.user = await LoginModel.create(this.body)
-        } catch(e) {
-            console.log(e)
-        }
+        this.user = await LoginModel.create(this.body)
+           
     }
 
     // Verificar se há 2 contas com o mesmo email
     async userExists() {
-       const user =  await LoginModel.findOne({email: this.body.email});
+        this.user =  await LoginModel.findOne({email: this.body.email});
+        if(this.user) this.errors.push('Usuário já existe.')
        
-       if(user) this.errors.push('Usuário já existe.')
     }
 
     valida() {
