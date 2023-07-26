bcd18d7febe015b59ce5e7761c421b67c0a8d799
bielgeri <bielgeri@gmail.com>
2023-07-26 09:57:54 -0300

    Cadastrando usuários (E)

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
+const contatoController = require('./src/controllers/contatoController')
+
+const { loginRequired } = require('./src/middlewares/middleware')
 
 
 // Rotas da home
@@ -12,4 +16,10 @@ route.get('/login/index', loginController.index)
 route.post('/login/register', loginController.register)
 route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
+
+//Rotas de contatos
+route.get('/contato/index', loginRequired, contatoController.index)
+route.post('/contato/register', loginRequired, contatoController.register)
+route.get('/contanto/index/:id', loginRequired, contatoController.editIndex);
+
 module.exports = route;
