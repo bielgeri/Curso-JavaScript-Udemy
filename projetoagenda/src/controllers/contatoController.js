c8a9fc5c83ee01b8481eb7c54cbf027f14e374c9
bielgeri <bielgeri@gmail.com>
2023-07-27 09:23:35 -0300

    editando contatos

--- projetoagenda/src/controllers/contatoController.js
+++ projetoagenda/src/controllers/contatoController.js
@@ -1,8 +1,11 @@
+const { async } = require('regenerator-runtime');
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
-    res.render('contato')
-    contato: {}
+    res.render('contato', {
+        contato: {}
+    })
+
 };
 exports.register = async (req, res) => {
     try {
@@ -31,4 +34,25 @@ exports.editIndex = async function (req, res) {
     if(!contato) return res.render('404');
 
     res.render('contato', { contato });
+};
+
+exports.edit = async function(req, res) {
+    try {
+        if(!req.params.id) return res.render('404');
+        const contato = new Contato(req.body)
+        await contato.edit(req.params.id);
+
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+
+        req.flash('success', 'Contato editado com sucesso');
+            req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+            return;
+    } catch(e) {
+        console.log(e)
+        res.render('404');
+    }
 };
