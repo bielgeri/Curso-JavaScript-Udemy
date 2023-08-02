a17005d544121f12c2192934162188ee085e39c5
bielgeri <bielgeri@gmail.com>
2023-08-02 08:04:50 -0300

    validando formulário

--- projetoagenda/frontend/main.js
+++ projetoagenda/frontend/main.js
@@ -1,5 +1,12 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 
-import './assets/css/style.css';
+import Login from './modules/Login';
+
+const cadastro = new Login('.form-cadastro')
+const login = new Login('.form-login')
+login.init();
+cadastro.init();
+
+// import './assets/css/style.css';
 
