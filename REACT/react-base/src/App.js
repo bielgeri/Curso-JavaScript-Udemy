0c14641af6a6e6e1233c1b3556de3a2dfd5254fd
Gabriel Rodrigues <bielgeri@gmail.com>
2024-01-18 08:09:31 -0300

    'Menu-da-aplicação'

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
