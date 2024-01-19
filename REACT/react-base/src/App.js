067fe5ce49084ae1ad813bf9631e9981eff63713
Gabriel Rodrigues <bielgeri@gmail.com>
2024-01-19 09:01:49 -0300

    router e history

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import Login from './pages/Login';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
