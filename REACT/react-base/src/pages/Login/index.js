9c3039db42557f767c7cf8567903ed48fc447291
Gabriel Rodrigues <bielgeri@gmail.com>
2024-01-29 08:08:55 -0300

    configurando axios (I)

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -3,7 +3,17 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title>
